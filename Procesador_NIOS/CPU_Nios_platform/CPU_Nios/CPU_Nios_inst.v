7ee170dae8c3f20066fd44e25da704284c1e9060
Joseph Jimenez <josephdjz@hotmail.com>
2024-08-26 18:33:19 -0600

    Delete Procesador_NIOS directory

--- Procesador_NIOS/CPU_Nios_platform/CPU_Nios/CPU_Nios_inst.v
+++
@@ -1,6 +0,0 @@
-	CPU_Nios u0 (
-		.clk_clk       (<connected-to-clk_clk>),       //   clk.clk
-		.leds_export   (<connected-to-leds_export>),   //  leds.export
-		.reset_reset_n (<connected-to-reset_reset_n>)  // reset.reset_n
-	);
-
